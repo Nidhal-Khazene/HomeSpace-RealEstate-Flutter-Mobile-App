c147e3ee5e16b97bd71f1f5a5f9e8ff303861eda
nidhal khazene <nidhal.khazene@gmail.com>
2025-11-10 10:41:39 +0100

    add font styles to the app

---
+++ lib/core/utils/styles.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+abstract class AppStyles {
+  final TextStyle medium32 = const TextStyle(
+    fontSize: 32,
+    fontWeight: FontWeight.w500,
+  );
+  final TextStyle regular32 = const TextStyle(
+    fontSize: 32,
+    fontWeight: FontWeight.w400,
+  );
+
+  final TextStyle medium24 = const TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w500,
+  );
+
+  final TextStyle regular24 = const TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w400,
+  );
+
+  final TextStyle medium16 = const TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+  );
+
+  final TextStyle regular16 = const TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+  );
+
+  final TextStyle medium12 = const TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w500,
+  );
+
+  final TextStyle regular12 = const TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+  );
+}
