c33b3e6311f44ea34089c123e409708e709ae5fd
nidhal khazene <nidhal.khazene@gmail.com>
2025-11-10 10:43:19 +0100

    add GT-Walsheim as the font of the app in theme data

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:home_space/constants.dart';
 import 'package:home_space/core/helper/on_generate_route.dart';
 import 'package:home_space/features/splash/presentation/views/splash_view.dart';
 
@@ -11,7 +12,8 @@ class HomeSpace extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      theme: ThemeData(fontFamily: kPrimaryFont),
       debugShowCheckedModeBanner: false,
       onGenerateRoute: onGenerateRoutes,
       initialRoute: SplashView.routeName,
