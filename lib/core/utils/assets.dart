17e2e339a7785c8b7be72bae9999fd5a3802e8b5
nidhal khazene <nidhal.khazene@gmail.com>
2025-11-11 15:48:30 +0100

    add icons

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -2,6 +2,18 @@
 class Assets {
   Assets._();
   
+  /// Assets for assetsImagesIconsApple
+  /// assets/images/icons/apple.png
+  static const String assetsImagesIconsApple = "assets/images/icons/apple.png";
+
+  /// Assets for assetsImagesIconsFacebook
+  /// assets/images/icons/facebook.png
+  static const String assetsImagesIconsFacebook = "assets/images/icons/facebook.png";
+
+  /// Assets for assetsImagesIconsGoogle
+  /// assets/images/icons/google.png
+  static const String assetsImagesIconsGoogle = "assets/images/icons/google.png";
+
   /// Assets for assetsImagesOnBoardingImage
   /// assets/images/on_boarding_image.jpg
   static const String assetsImagesOnBoardingImage = "assets/images/on_boarding_image.jpg";
