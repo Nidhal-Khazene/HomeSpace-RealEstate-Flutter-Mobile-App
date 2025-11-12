669ef4478ec2e502197458ddd4e5552c89baf84f
nidhal khazene <nidhal.khazene@gmail.com>
2025-11-12 13:42:44 +0100

    add text theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:home_space/constants.dart';
-import 'package:home_space/core/helper/on_generate_route.dart';
+import 'package:home_space/core/routing/on_generate_route.dart';
 import 'package:home_space/features/splash/presentation/views/splash_view.dart';
 
 void main() {
@@ -13,7 +13,14 @@ class HomeSpace extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(fontFamily: kPrimaryFont),
+      theme: ThemeData(
+        fontFamily: kPrimaryFont,
+        textTheme: TextTheme(
+          bodyLarge: TextStyle(color: kPrimaryColorFont),
+          bodyMedium: TextStyle(color: kPrimaryColorFont),
+          bodySmall: TextStyle(color: kPrimaryColorFont),
+        ),
+      ),
       debugShowCheckedModeBanner: false,
       onGenerateRoute: onGenerateRoutes,
       initialRoute: SplashView.routeName,
