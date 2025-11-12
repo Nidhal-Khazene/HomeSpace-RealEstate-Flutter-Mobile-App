722badb14e0260d99b82d59caaa9b372226c8ef2
nidhal khazene <nidhal.khazene@gmail.com>
2025-11-12 16:16:37 +0100

    add turn on notification route

--- lib/core/routing/on_generate_route.dart
+++ lib/core/routing/on_generate_route.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:home_space/features/auth/presentation/views/finish_sign_up_view.dart';
 import 'package:home_space/features/auth/presentation/views/login_view.dart';
 import 'package:home_space/features/auth/presentation/views/widgets/login_or_sign_up_password.dart';
+import 'package:home_space/features/auth/presentation/views/widgets/turn_on_notification_view.dart';
 import 'package:home_space/features/on_boarding/presentation/views/on_boarding_view.dart';
 import 'package:home_space/features/splash/presentation/views/splash_view.dart';
 
@@ -19,6 +20,10 @@ Route<dynamic> onGenerateRoutes(RouteSettings settings) {
       );
     case FinishSignUpView.routeName:
       return MaterialPageRoute(builder: (context) => const FinishSignUpView());
+    case TurnOnNotification.routeName:
+      return MaterialPageRoute(
+        builder: (context) => const TurnOnNotification(),
+      );
     default:
       return MaterialPageRoute(builder: (context) => const SplashView());
   }
