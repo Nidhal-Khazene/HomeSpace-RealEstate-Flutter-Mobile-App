e8e3d208c079a2a1740f8806c5b879ece7599334
nidhal khazene <nidhal.khazene@gmail.com>
2025-11-12 12:10:25 +0100

    add custom arrow back button

---
+++ lib/core/widgets/custom_arrow_back_button.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:home_space/core/utils/assets.dart';
+
+import '../../constants.dart';
+
+class CustomArrowBackButton extends StatelessWidget {
+  const CustomArrowBackButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 16),
+      child: Container(
+        decoration: const ShapeDecoration(
+          shape: OvalBorder(side: BorderSide(color: Color(0xFFE9E8E8))),
+        ),
+        height: 40,
+        width: 40,
+        child: Center(
+          child: SvgPicture.asset(
+            Assets.assetsImagesIconsArrowBack,
+            colorFilter: ColorFilter.mode(kPrimaryColorFont, BlendMode.srcIn),
+          ),
+        ),
+      ),
+    );
+  }
+}
