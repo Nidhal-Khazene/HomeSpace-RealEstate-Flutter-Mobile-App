23616fa81f79516c60144cd025bb32dfaa962ab8
nidhal khazene <nidhal.khazene@gmail.com>
2025-11-12 13:43:31 +0100

    add finish sign up view

--- lib/features/auth/presentation/views/finish_sign_up_view.dart
+++ lib/features/auth/presentation/views/finish_sign_up_view.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:home_space/features/auth/presentation/views/widgets/finish_sign_up_view_body.dart';
 
-class FinishSignUp extends StatelessWidget {
-  const FinishSignUp({super.key});
+class FinishSignUpView extends StatelessWidget {
+  const FinishSignUpView({super.key});
+
   static const routeName = "finishSignUp";
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: ,);
+    return const Scaffold(body: FinishSignUpViewBody());
   }
 }
