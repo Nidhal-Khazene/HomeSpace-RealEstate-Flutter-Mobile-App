68e88191aac4274e9052e86a9a2a1ae8448cb7f7
nidhal khazene <nidhal.khazene@gmail.com>
2025-11-12 12:24:15 +0100

    add success and arrow back to assets

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -6,6 +6,10 @@ class Assets {
   /// assets/images/icons/apple.png
   static const String assetsImagesIconsApple = "assets/images/icons/apple.png";
 
+  /// Assets for assetsImagesIconsArrowBack
+  /// assets/images/icons/arrow_back.svg
+  static const String assetsImagesIconsArrowBack = "assets/images/icons/arrow_back.svg";
+
   /// Assets for assetsImagesIconsFacebook
   /// assets/images/icons/facebook.png
   static const String assetsImagesIconsFacebook = "assets/images/icons/facebook.png";
@@ -13,6 +17,10 @@ class Assets {
   /// Assets for assetsImagesIconsGoogle
   /// assets/images/icons/google.png
   static const String assetsImagesIconsGoogle = "assets/images/icons/google.png";
+
+  /// Assets for assetsImagesIconsSuccessButton
+  /// assets/images/icons/success_button.svg
+  static const String assetsImagesIconsSuccessButton = "assets/images/icons/success_button.svg";
 
   /// Assets for assetsImagesOnBoardingImage
   /// assets/images/on_boarding_image.jpg
