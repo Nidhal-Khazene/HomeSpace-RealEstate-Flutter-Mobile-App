fd45639c53d6ec8b81da7ab9a385679ae3999e88
nidhal khazene <nidhal.khazene@gmail.com>
2025-11-12 11:42:52 +0100

    add textContent attribute to custom text field

--- lib/core/widgets/custom_text_field.dart
+++ lib/core/widgets/custom_text_field.dart
@@ -2,13 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:home_space/core/utils/styles.dart';
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField({super.key});
+  const CustomTextField({super.key, required this.textContent});
+  final String textContent;
 
   @override
   Widget build(BuildContext context) {
     return TextField(
+      keyboardType: TextInputType.emailAddress,
       decoration: InputDecoration(
-        hintText: "Email Address",
+        hintText: textContent,
         hintStyle: AppStyles.regular16.copyWith(color: const Color(0xFF8C8B8B)),
         enabledBorder: buildOutlineInputBorder(),
         focusedBorder: buildOutlineInputBorder(),
