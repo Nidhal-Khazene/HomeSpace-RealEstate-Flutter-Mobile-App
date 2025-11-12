7dac6c66683b51fc7e03ec9ceb3c346a127383d1
nidhal khazene <nidhal.khazene@gmail.com>
2025-11-12 13:44:11 +0100

    add finish sign up view body stature

---
+++ lib/features/auth/presentation/views/widgets/finish_sign_up_view_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FinishSignUpViewBody extends StatelessWidget {
+  const FinishSignUpViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
