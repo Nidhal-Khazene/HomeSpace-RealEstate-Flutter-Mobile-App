48c92756efd48783b078328c790c2b4232654fe8
nidhal khazene <nidhal.khazene@gmail.com>
2025-11-13 20:40:23 +0100

    implement onChanged in finish sign up view body part 1

--- lib/features/auth/presentation/views/widgets/finish_sign_up_view_body.dart
+++ lib/features/auth/presentation/views/widgets/finish_sign_up_view_body.dart
@@ -8,8 +8,15 @@ import 'package:home_space/core/widgets/custom_text_field.dart';
 import 'package:home_space/features/auth/presentation/views/widgets/login_or_sign_up_password.dart';
 import 'package:home_space/features/auth/presentation/views/widgets/turn_on_notification_view.dart';
 
-class FinishSignUpViewBody extends StatelessWidget {
+class FinishSignUpViewBody extends StatefulWidget {
   const FinishSignUpViewBody({super.key});
+
+  @override
+  State<FinishSignUpViewBody> createState() => _FinishSignUpViewBodyState();
+}
+
+class _FinishSignUpViewBodyState extends State<FinishSignUpViewBody> {
+  bool readyToContinue = false;
 
   @override
   Widget build(BuildContext context) {
@@ -30,16 +37,25 @@ class FinishSignUpViewBody extends StatelessWidget {
           const SizedBox(height: 35),
           Text("Finish Signup", style: AppStyles.medium32),
           const SizedBox(height: 32),
-          const CustomTextField(textContent: "Firstname"),
+          CustomTextField(
+            textContent: "Firstname",
+            onChanged: (data) => onChangedLogic(data, "first"),
+          ),
           const SizedBox(height: 12),
-          const CustomTextField(textContent: "Lastname"),
+          CustomTextField(
+            textContent: "Lastname",
+            onChanged: (data) => onChangedLogic(data, "last"),
+          ),
           const SizedBox(height: 12),
           CustomTextField(
             textContent: "Katrinabennet21@gmail.com",
             textContentColor: kPrimaryColorFont,
           ),
           const SizedBox(height: 12),
-          const CustomTextField(textContent: "Password"),
+          CustomTextField(
+            textContent: "Password",
+            onChanged: (data) => onChangedLogic(data, "password"),
+          ),
           const SizedBox(height: 12),
           Text("Must be 8 characters", style: AppStyles.regular12),
           const SizedBox(height: 32),
@@ -47,7 +63,7 @@ class FinishSignUpViewBody extends StatelessWidget {
             TextSpan(
               children: [
                 TextSpan(
-                  text: "By selecting agree and Continue , you accept our ",
+                  text: "By selecting agree and Continue, you accept our ",
                   style: AppStyles.regular14,
                 ),
                 TextSpan(
@@ -66,13 +82,32 @@ class FinishSignUpViewBody extends StatelessWidget {
               Navigator.popAndPushNamed(context, TurnOnNotification.routeName);
             },
             child: CustomButton(
-              textContentColor: ColorsData.kPrimaryColor,
+              textContentColor: readyToContinue
+                  ? Colors.white
+                  : ColorsData.kPrimaryColor,
               textContent: "Agree and Continue",
-              colorBackground: Color(0xffF2F4FD),
+              colorBackground: readyToContinue
+                  ? ColorsData.kPrimaryColor
+                  : const Color(0xffF2F4FD),
             ),
           ),
         ],
       ),
     );
+  }
+
+  String firstName = '';
+  String lastName = '';
+  String password = '';
+
+  void onChangedLogic(String? data, String fieldKey) {
+    if (fieldKey == 'first') firstName = data ?? '';
+    if (fieldKey == 'last') lastName = data ?? '';
+    if (fieldKey == 'password') password = data ?? '';
+
+    setState(() {
+      readyToContinue =
+          firstName.isNotEmpty && lastName.isNotEmpty && password.isNotEmpty;
+    });
   }
 }
