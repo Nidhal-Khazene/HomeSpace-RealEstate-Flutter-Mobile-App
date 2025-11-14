55b3cceeffe9f067a08bf2cd5ef222767b4aad31
nidhal khazene <nidhal.khazene@gmail.com>
2025-11-14 21:16:18 +0100

    add Gesture detactor and onTap attribute to custom button

--- lib/core/widgets/custom_button.dart
+++ lib/core/widgets/custom_button.dart
@@ -9,27 +9,32 @@ class CustomButton extends StatelessWidget {
     required this.textContent,
     this.textContentColor,
     this.borderColor,
+    this.onTap,
   });
   final Color? colorBackground;
   final String textContent;
   final Color? textContentColor;
   final Color? borderColor;
+  final void Function()? onTap;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(vertical: 16),
-      width: double.infinity,
-      decoration: BoxDecoration(
-        border: Border.all(color: borderColor ?? ColorsData.kPrimaryColor),
-        borderRadius: BorderRadius.circular(100),
-        color: colorBackground ?? ColorsData.kPrimaryColor,
-      ),
-      child: Text(
-        textContent,
-        textAlign: TextAlign.center,
-        style: AppStyles.regular16.copyWith(
-          color: textContentColor ?? Colors.white,
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.symmetric(vertical: 16),
+        width: double.infinity,
+        decoration: BoxDecoration(
+          border: Border.all(color: borderColor ?? ColorsData.kPrimaryColor),
+          borderRadius: BorderRadius.circular(100),
+          color: colorBackground ?? ColorsData.kPrimaryColor,
+        ),
+        child: Text(
+          textContent,
+          textAlign: TextAlign.center,
+          style: AppStyles.regular16.copyWith(
+            color: textContentColor ?? Colors.white,
+          ),
         ),
       ),
     );
