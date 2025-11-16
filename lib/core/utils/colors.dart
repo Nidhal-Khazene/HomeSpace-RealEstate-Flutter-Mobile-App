3a1adeef81043b4f12d22584668eb5be63328797
nidhal khazene <nidhal.khazene@gmail.com>
2025-11-16 10:08:17 +0100

    add primary and secodary color font to colors data

--- lib/core/utils/colors.dart
+++ lib/core/utils/colors.dart
@@ -3,4 +3,6 @@ import 'package:flutter/cupertino.dart';
 abstract class ColorsData {
   static final kPrimaryColor = const Color(0xff405AE4);
   static final kNotReadyButtonColor = const Color(0xFFF2F4FC);
+  static final Color kPrimaryColorFont = const Color(0xFF1F1F1F);
+  static final Color kSecondaryColorFont = const Color(0xFF8C8B8B);
 }
