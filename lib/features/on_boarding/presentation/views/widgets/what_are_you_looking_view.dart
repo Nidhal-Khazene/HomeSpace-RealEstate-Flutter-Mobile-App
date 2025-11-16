99e0175ab2064e69ce16218a763d87c447f2a00f
nidhal khazene <nidhal.khazene@gmail.com>
2025-11-16 13:38:02 +0100

    what are you looking view part 2

--- lib/features/on_boarding/presentation/views/widgets/what_are_you_looking_view.dart
+++ lib/features/on_boarding/presentation/views/widgets/what_are_you_looking_view.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:home_space/core/utils/assets.dart';
 import 'package:home_space/core/utils/colors.dart';
 import 'package:home_space/core/utils/styles.dart';
+import 'package:home_space/core/widgets/custom_search_bar.dart';
 
 class WhatAreYouLookingView extends StatelessWidget {
   const WhatAreYouLookingView({super.key});
@@ -18,6 +21,24 @@ class WhatAreYouLookingView extends StatelessWidget {
           style: AppStyles.regular12.copyWith(
             color: ColorsData.kSecondaryColorFont,
           ),
+        ),
+        const SizedBox(height: 46),
+        const CustomSearchBar(
+          textContent: "Search Address, City or Postal code",
+        ),
+        const SizedBox(height: 37),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SvgPicture.asset(Assets.assetsImagesIconsLocationAddedIcon),
+            const SizedBox(width: 8),
+            Text(
+              "Use Current Location",
+              style: AppStyles.regular12.copyWith(
+                color: ColorsData.kPrimaryColor,
+              ),
+            ),
+          ],
         ),
       ],
     );
