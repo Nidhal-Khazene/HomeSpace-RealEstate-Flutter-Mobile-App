074c35775487860f468550042b08d4f3cd4958ad
nidhal khazene <nidhal.khazene@gmail.com>
2025-11-16 16:20:25 +0100

    add custom price field

---
+++ lib/core/widgets/custom_price_field.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:home_space/core/utils/colors.dart';
+import 'package:home_space/core/utils/styles.dart';
+
+class CustomPriceField extends StatelessWidget {
+  const CustomPriceField({super.key, required this.textContent});
+  final String textContent;
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      keyboardType: TextInputType.number,
+      decoration: InputDecoration(
+        hintText: textContent,
+        hintStyle: AppStyles.regular16.copyWith(
+          color: ColorsData.kSecondaryColorFont,
+        ),
+        enabledBorder: buildOutlineInputBorder(),
+        disabledBorder: buildOutlineInputBorder(),
+        border: buildOutlineInputBorder(),
+        focusedBorder: buildOutlineInputBorder(),
+      ),
+    );
+  }
+
+  OutlineInputBorder buildOutlineInputBorder() {
+    return OutlineInputBorder(
+      borderRadius: BorderRadius.circular(4),
+      borderSide: const BorderSide(color: Color(0xFFE9E8E8), width: 1),
+    );
+  }
+}
