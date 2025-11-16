8e676f1b738b5006e4b4ae781c4d74569e9d26a3
nidhal khazene <nidhal.khazene@gmail.com>
2025-11-16 21:22:17 +0100

    add secondary color

--- lib/core/utils/colors.dart
+++ lib/core/utils/colors.dart
@@ -2,7 +2,8 @@ import 'package:flutter/cupertino.dart';
 
 abstract class ColorsData {
   static final kPrimaryColor = const Color(0xff405AE4);
+  static final Color kSecondaryColor = const Color(0xFF4360F6);
   static final kNotReadyButtonColor = const Color(0xFFF2F4FC);
-  static final Color kPrimaryColorFont = const Color(0xFF1F1F1F);
-  static final Color kSecondaryColorFont = const Color(0xFF8C8B8B);
+  static final Color kFontPrimaryColor = const Color(0xFF1F1F1F);
+  static final Color kFontSecondaryColor = const Color(0xFF8C8B8B);
 }
