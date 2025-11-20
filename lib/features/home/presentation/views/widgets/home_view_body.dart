7314f0f782ad939191fd6646db0435de4adb230c
nidhal khazene <nidhal.khazene@gmail.com>
2025-11-20 16:39:54 +0100

    add images assets for main home view

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:home_space/features/home/presentation/views/widgets/home_header.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Column(children: [HomeHeader()]);
   }
 }
