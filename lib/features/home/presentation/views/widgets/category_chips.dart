b2f66add4abecc3a7c2f514f7ce6d0e9ccf864ec
nidhal khazene <nidhal.khazene@gmail.com>
2025-11-23 10:39:04 +0100

    add category chips

---
+++ lib/features/home/presentation/views/widgets/category_chips.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:home_space/core/utils/styles.dart';
+
+import '../../../../../core/utils/colors.dart';
+
+class CategoryChips extends StatefulWidget {
+  const CategoryChips({super.key});
+
+  @override
+  State<CategoryChips> createState() => _CategoryChipsState();
+}
+
+class _CategoryChipsState extends State<CategoryChips> {
+  int _selectedIndex = 0;
+
+  final List<String> categories = [
+    "Apartment",
+    "House",
+    "Villa",
+    "House",
+    "Villa",
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 50, // fixed height for horizontal chips
+      child: ListView.separated(
+        scrollDirection: Axis.horizontal,
+        itemCount: categories.length,
+        separatorBuilder: (_, __) => const SizedBox(width: 16),
+        itemBuilder: (context, index) {
+          final bool isSelected = _selectedIndex == index;
+
+          return ChoiceChip(
+            pressElevation: 0,
+            elevation: 0,
+            showCheckmark: false,
+            label: Text(categories[index]),
+            selected: isSelected,
+            onSelected: (_) {
+              setState(() {
+                _selectedIndex = index;
+              });
+            },
+            backgroundColor: Colors.transparent,
+            selectedColor: ColorsData.kSecondaryColor,
+            labelStyle: AppStyles.regular12.copyWith(
+              color: isSelected ? Colors.white : ColorsData.kFontSecondaryColor,
+            ),
+            shape: RoundedRectangleBorder(
+              side: BorderSide(
+                color: isSelected
+                    ? ColorsData.kSecondaryColor
+                    : ColorsData.kBorderColor,
+              ),
+              borderRadius: BorderRadius.circular(33),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:home_space/core/utils/styles.dart';
 import 'package:home_space/core/widgets/custom_search_bar.dart';
+import 'package:home_space/features/home/presentation/views/widgets/category_chips.dart';
 import 'package:home_space/features/home/presentation/views/widgets/home_header.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -19,6 +20,8 @@ class HomeViewBody extends StatelessWidget {
         ),
         const SizedBox(height: 32),
         Text("Listings Nearby", style: AppStyles.medium16),
+        const SizedBox(height: 16),
+        const CategoryChips(),
       ],
     );
   }
