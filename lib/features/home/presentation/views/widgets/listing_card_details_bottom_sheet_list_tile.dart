48260b3b2599b1395bc13737e3e056ed8e7596ad
nidhal khazene <nidhal.khazene@gmail.com>
2025-11-29 16:29:59 +0100

    extract listing_card_details_bottom_sheet_list_tile

---
+++ lib/features/home/presentation/views/widgets/listing_card_details_bottom_sheet_list_tile.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:iconsax_flutter/iconsax_flutter.dart';
+
+import '../../../../../core/utils/colors.dart';
+import '../../../../../core/utils/styles.dart';
+
+class ListingCardDetailsCustomBottomSheetListTile extends StatelessWidget {
+  const ListingCardDetailsCustomBottomSheetListTile({
+    super.key,
+    required this.title,
+  });
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      contentPadding: const EdgeInsets.symmetric(vertical: 22, horizontal: 12),
+      shape: RoundedRectangleBorder(
+        side: BorderSide(color: ColorsData.kBorderColor, width: 1),
+      ),
+      leading: Icon(
+        Iconsax.calendar_1_copy,
+        color: ColorsData.kSecondaryColor,
+        size: 20,
+      ),
+      title: Text(title, style: AppStyles.regular16),
+      trailing: Icon(
+        Icons.arrow_forward_ios,
+        color: ColorsData.kFontPrimaryColor,
+        size: 24,
+      ),
+    );
+  }
+}
