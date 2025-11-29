c6ceec33b15744f2d739828f30e6c5757207310c
nidhal khazene <nidhal.khazene@gmail.com>
2025-11-29 15:12:56 +0100

    add primary screen padding

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
 const String kPrimaryFont = "GT-Walsheim";
 const double kHorizontalPadding = 24;
+const EdgeInsetsGeometry kPrimaryScreenPadding = EdgeInsets.only(
+  left: 24,
+  right: 24,
+  top: 16,
+);
 const double kBottomPadding = 32;
 const int kMillisecondsDuration = 300;
 const String kIsOnBoardingViewSeen = "isOnBoardingViewSeen";
