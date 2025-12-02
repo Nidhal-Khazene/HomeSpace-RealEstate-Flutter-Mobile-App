c799c4f5c322a1e0ab6447520d836a291e83c32b
nidhal khazene <nidhal.khazene@gmail.com>
2025-12-02 09:58:52 +0100

    schedule tour view: add custom button

--- lib/features/home/presentation/views/widgets/schedule_tour_view.dart
+++ lib/features/home/presentation/views/widgets/schedule_tour_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:home_space/constants.dart';
 import 'package:home_space/core/utils/styles.dart';
 import 'package:home_space/core/widgets/custom_app_bar.dart';
+import 'package:home_space/core/widgets/custom_button.dart';
 import 'package:home_space/features/home/presentation/views/widgets/select_time_choice_chip_list_view.dart';
 
 class ScheduleTourView extends StatelessWidget {
@@ -15,24 +16,24 @@ class ScheduleTourView extends StatelessWidget {
       body: SafeArea(
         child: Padding(
           padding: kPrimaryScreenPadding,
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                CustomAppBar(
-                  title: "Schedule Tour",
-                  widthFromBackButton: 77,
-                  textStyle: AppStyles.medium16,
-                ),
-                const SizedBox(height: 32),
-                Text("Select date", style: AppStyles.medium16),
-                const SizedBox(height: 16),
-                const SizedBox(height: 48),
-                Text("Select Time", style: AppStyles.medium16),
-                const SizedBox(height: 24),
-                SelectTimeChoiceChipListView(),
-              ],
-            ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CustomAppBar(
+                title: "Schedule Tour",
+                widthFromBackButton: 77,
+                textStyle: AppStyles.medium16,
+              ),
+              const SizedBox(height: 32),
+              Text("Select date", style: AppStyles.medium16),
+              const SizedBox(height: 16),
+              const SizedBox(height: 48),
+              Text("Select Time", style: AppStyles.medium16),
+              const SizedBox(height: 24),
+              const SelectTimeChoiceChipListView(),
+              const Flexible(child: SizedBox(height: 134)),
+              const CustomButton(textContent: "Schedule"),
+            ],
           ),
         ),
       ),
