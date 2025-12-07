53209ab4400809270f13e97bf4890bc5897896a5
nidhal khazene <nidhal.khazene@gmail.com>
2025-12-07 15:32:26 +0100

    add onTap method in favourites list tile item

--- lib/features/favourites/presentation/views/widgets/favourites_list_tile_item.dart
+++ lib/features/favourites/presentation/views/widgets/favourites_list_tile_item.dart
@@ -12,45 +12,50 @@ class FavouritesListTileItem extends StatelessWidget {
     super.key,
     required this.title,
     required this.subtitle,
+    this.onTap,
   });
 
   final String title, subtitle;
+  final void Function()? onTap;
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(8),
-        side: BorderSide(color: ColorsData.kBorderColor),
-      ),
-      leading: Container(
-        width: 40,
-        height: 40,
-        padding: const EdgeInsets.all(10),
-        decoration: ShapeDecoration(
-          color: const Color(0xFFE8ECFF) /* Primary-Red-10 */,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(100),
+    return GestureDetector(
+      onTap: onTap,
+      child: ListTile(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8),
+          side: BorderSide(color: ColorsData.kBorderColor),
+        ),
+        leading: Container(
+          width: 40,
+          height: 40,
+          padding: const EdgeInsets.all(10),
+          decoration: ShapeDecoration(
+            color: const Color(0xFFE8ECFF) /* Primary-Red-10 */,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(100),
+            ),
+          ),
+          child: SvgPicture.asset(
+            Assets.assetsImagesIconsHeartWhiteOutlineIcon,
+            colorFilter: ColorFilter.mode(
+              ColorsData.kPrimaryColor,
+              BlendMode.srcIn,
+            ),
           ),
         ),
-        child: SvgPicture.asset(
-          Assets.assetsImagesIconsHeartWhiteOutlineIcon,
-          colorFilter: ColorFilter.mode(
-            ColorsData.kPrimaryColor,
-            BlendMode.srcIn,
+        title: Text(title, style: AppStyles.regular16),
+        subtitle: Text(
+          subtitle,
+          style: AppStyles.regular12.copyWith(
+            color: ColorsData.kFontSecondaryColor,
           ),
         ),
-      ),
-      title: Text(title, style: AppStyles.regular16),
-      subtitle: Text(
-        subtitle,
-        style: AppStyles.regular12.copyWith(
-          color: ColorsData.kFontSecondaryColor,
+        trailing: Transform.rotate(
+          angle: math.pi / 2,
+          child: const CustomThreeDots(),
         ),
-      ),
-      trailing: Transform.rotate(
-        angle: math.pi / 2,
-        child: const CustomThreeDots(),
       ),
     );
   }
