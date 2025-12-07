7b0dc7e57d4d2f19dd812607babb701c02c18021
nidhal khazene <nidhal.khazene@gmail.com>
2025-12-07 11:20:30 +0100

    add the header of the favourties view body

--- lib/features/favourites/presentation/views/widgets/favourites_view_body.dart
+++ lib/features/favourites/presentation/views/widgets/favourites_view_body.dart
@@ -1,14 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:home_space/constants.dart';
+import 'package:home_space/core/utils/colors.dart';
+import 'package:home_space/core/utils/styles.dart';
 
 class FavouritesViewBody extends StatelessWidget {
   const FavouritesViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
+    return Padding(
       padding: kPrimaryScreenPadding,
-      child: Column(children: []),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text("Your List", style: AppStyles.medium16),
+              Text(
+                "New List",
+                style: AppStyles.medium16.copyWith(
+                  color: ColorsData.kSecondaryColor,
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 24),
+        ],
+      ),
     );
   }
 }
