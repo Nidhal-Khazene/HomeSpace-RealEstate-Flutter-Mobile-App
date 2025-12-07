b151604fd87cd9233f6c8b60600a883ed3e85945
nidhal khazene <nidhal.khazene@gmail.com>
2025-12-07 14:49:23 +0100

    favourites list tile item added and favourites biew done

---
+++ lib/features/favourites/presentation/views/widgets/favourites_list_tile_item.dart
@@ -0,0 +1,57 @@
+import 'dart:math' as math;
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:home_space/core/utils/assets.dart';
+import 'package:home_space/core/utils/colors.dart';
+import 'package:home_space/core/utils/styles.dart';
+import 'package:home_space/core/widgets/custom_three_dots.dart';
+
+class FavouritesListTileItem extends StatelessWidget {
+  const FavouritesListTileItem({
+    super.key,
+    required this.title,
+    required this.subtitle,
+  });
+
+  final String title, subtitle;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8),
+        side: BorderSide(color: ColorsData.kBorderColor),
+      ),
+      leading: Container(
+        width: 40,
+        height: 40,
+        padding: const EdgeInsets.all(10),
+        decoration: ShapeDecoration(
+          color: const Color(0xFFE8ECFF) /* Primary-Red-10 */,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(100),
+          ),
+        ),
+        child: SvgPicture.asset(
+          Assets.assetsImagesIconsHeartWhiteOutlineIcon,
+          colorFilter: ColorFilter.mode(
+            ColorsData.kPrimaryColor,
+            BlendMode.srcIn,
+          ),
+        ),
+      ),
+      title: Text(title, style: AppStyles.regular16),
+      subtitle: Text(
+        subtitle,
+        style: AppStyles.regular12.copyWith(
+          color: ColorsData.kFontSecondaryColor,
+        ),
+      ),
+      trailing: Transform.rotate(
+        angle: math.pi / 2,
+        child: const CustomThreeDots(),
+      ),
+    );
+  }
+}
--- lib/features/favourites/presentation/views/widgets/favourites_view_body.dart
+++ lib/features/favourites/presentation/views/widgets/favourites_view_body.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:home_space/constants.dart';
+import 'package:home_space/core/models/list_tile_model.dart';
 import 'package:home_space/core/utils/colors.dart';
 import 'package:home_space/core/utils/styles.dart';
+import 'package:home_space/features/favourites/presentation/views/widgets/favourites_list_tile_item.dart';
 
 class FavouritesViewBody extends StatelessWidget {
   const FavouritesViewBody({super.key});
+
+  static final List<ListTileModel> list = [
+    ListTileModel(title: "Apartment in Uk", subtitle: "10 Locations"),
+    ListTileModel(title: "Rental in Florida", subtitle: "5 Locations"),
+    ListTileModel(title: "New listings", subtitle: "24 Locations"),
+    ListTileModel(title: "Apartment in Chicago", subtitle: "10 Locations"),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -25,6 +34,16 @@ class FavouritesViewBody extends StatelessWidget {
             ],
           ),
           const SizedBox(height: 24),
+          Column(
+            spacing: 12,
+            children: List.generate(
+              4,
+              (index) => FavouritesListTileItem(
+                title: list[index].title!,
+                subtitle: list[index].subtitle!,
+              ),
+            ),
+          ),
         ],
       ),
     );
