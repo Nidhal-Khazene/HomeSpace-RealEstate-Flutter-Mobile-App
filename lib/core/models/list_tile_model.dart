da0edcb1562bbf5d89d8c98deccc11c7d6e3731b
nidhal khazene <nidhal.khazene@gmail.com>
2026-01-07 20:37:25 +0100

    list tile model: add new attributes

--- lib/core/models/list_tile_model.dart
+++ lib/core/models/list_tile_model.dart
@@ -1,5 +1,15 @@
+import 'package:flutter/material.dart';
+
 class ListTileModel {
   final String? leading, title, subtitle, trailing;
+  final Widget? leadingWidget, trailingWidget;
 
-  ListTileModel({this.leading, this.title, this.subtitle, this.trailing});
+  ListTileModel({
+    this.leadingWidget,
+    this.trailingWidget,
+    this.leading,
+    this.title,
+    this.subtitle,
+    this.trailing,
+  });
 }
