fd2640939a3c81d83578271edb5ed63ec02ca896
nidhal khazene <nidhal.khazene@gmail.com>
2026-01-07 19:07:53 +0100

    settings view body part 1

--- lib/features/settings/presentation/views/widgets/settings_view_body.dart
+++ lib/features/settings/presentation/views/widgets/settings_view_body.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:home_space/core/utils/styles.dart';
 
 class SettingsViewBody extends StatelessWidget {
   const SettingsViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column();
+    return Column(
+      children: [
+        const SizedBox(height: 16),
+        Text("Settings", style: AppStyles.medium16),
+        const SizedBox(height: 24),
+      ],
+    );
   }
 }
