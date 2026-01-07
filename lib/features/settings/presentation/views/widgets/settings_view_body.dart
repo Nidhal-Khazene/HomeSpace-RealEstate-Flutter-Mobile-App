280e981f3984a8b791ecd3bbf5d4d8b786eebcce
nidhal khazene <nidhal.khazene@gmail.com>
2026-01-07 20:39:06 +0100

    settigns view body done

--- lib/features/settings/presentation/views/widgets/settings_view_body.dart
+++ lib/features/settings/presentation/views/widgets/settings_view_body.dart
@@ -1,16 +1,83 @@
 import 'package:flutter/material.dart';
+import 'package:home_space/core/models/list_tile_model.dart';
+import 'package:home_space/core/utils/colors.dart';
 import 'package:home_space/core/utils/styles.dart';
+import 'package:iconsax_flutter/iconsax_flutter.dart';
 
 class SettingsViewBody extends StatelessWidget {
   const SettingsViewBody({super.key});
 
+  static List<ListTileModel> list = [
+    ListTileModel(
+      title: "Edit Profile",
+      leadingWidget: Icon(
+        Iconsax.user_square_copy,
+        color: ColorsData.kMediumPrimaryColor,
+        size: 20,
+      ),
+    ),
+    ListTileModel(
+      title: "Mortgage Calculator",
+      leadingWidget: Icon(
+        Iconsax.calculator_copy,
+        color: ColorsData.kMediumPrimaryColor,
+        size: 20,
+      ),
+    ),
+    ListTileModel(
+      title: "Notifications",
+      leadingWidget: Icon(
+        Iconsax.notification_copy,
+        color: ColorsData.kMediumPrimaryColor,
+        size: 20,
+      ),
+    ),
+    ListTileModel(
+      title: "Help Center",
+      leadingWidget: Icon(
+        Iconsax.message_question_copy,
+        color: ColorsData.kMediumPrimaryColor,
+        size: 20,
+      ),
+    ),
+    ListTileModel(
+      title: "Contact Support",
+      leadingWidget: Icon(
+        Iconsax.support_copy,
+        color: ColorsData.kMediumPrimaryColor,
+        size: 20,
+      ),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         const SizedBox(height: 16),
         Text("Settings", style: AppStyles.medium16),
         const SizedBox(height: 24),
+        Column(
+          children: List.generate(
+            list.length,
+            (index) => Padding(
+              padding: const EdgeInsets.only(bottom: 12),
+              child: ListTile(
+                shape: RoundedRectangleBorder(
+                  side: BorderSide(color: ColorsData.kBorderColor),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                leading: CircleAvatar(
+                  backgroundColor: ColorsData.kLightPrimaryColor,
+                  child: list[index].leadingWidget,
+                ),
+                title: Text(list[index].title!, style: AppStyles.regular14),
+                trailing: const Icon(Icons.arrow_forward_ios, size: 10),
+              ),
+            ),
+          ),
+        ),
       ],
     );
   }
