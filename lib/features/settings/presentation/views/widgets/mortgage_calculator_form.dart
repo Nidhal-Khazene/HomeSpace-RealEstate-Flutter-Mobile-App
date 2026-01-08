55ffe706c5c0ae9349cdd61000e45103fe598e4c
nidhal khazene <nidhal.khazene@gmail.com>
2026-01-08 10:23:40 +0100

    mortgage_calculator_form done

--- lib/features/settings/presentation/views/widgets/mortgage_calculator_form.dart
+++ lib/features/settings/presentation/views/widgets/mortgage_calculator_form.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:home_space/constants.dart';
 import 'package:home_space/core/widgets/custom_app_bar.dart';
+import 'package:home_space/core/widgets/custom_button.dart';
+import 'package:home_space/core/widgets/custom_text_field.dart';
 
 class MortgageCalculatorForm extends StatelessWidget {
   const MortgageCalculatorForm({super.key});
@@ -17,6 +19,17 @@ class MortgageCalculatorForm extends StatelessWidget {
             children: [
               CustomAppBar(title: "Mortgage Calculator"),
               SizedBox(height: 16),
+              CustomTextField(textContent: "Home Price"),
+              SizedBox(height: 12),
+              CustomTextField(textContent: "Down payment"),
+              SizedBox(height: 12),
+              CustomTextField(textContent: "Loan term"),
+              SizedBox(height: 12),
+              CustomTextField(textContent: "Interest Rate"),
+              SizedBox(height: 12),
+              CustomTextField(textContent: "Zip code"),
+              Flexible(child: SizedBox(height: 222)),
+              CustomButton(textContent: "Continue"),
             ],
           ),
         ),
