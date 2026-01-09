1f72e1164899ac227b3737821d74af1cb4aac677
nidhal khazene <nidhal.khazene@gmail.com>
2026-01-09 21:35:08 +0100

    payment item model added

---
+++ lib/core/models/payment_item_model.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+class PaymentItemModel {
+  final Color color;
+  final String title, price;
+
+  PaymentItemModel({
+    required this.color,
+    required this.title,
+    required this.price,
+  });
+}
