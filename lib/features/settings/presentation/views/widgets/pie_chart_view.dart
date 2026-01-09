bd836ec9dfc890d558d4b753515e79eca6b171c6
nidhal khazene <nidhal.khazene@gmail.com>
2026-01-09 21:29:18 +0100

    pie chart view: add pie chart sections data

--- lib/features/settings/presentation/views/widgets/pie_chart_view.dart
+++ lib/features/settings/presentation/views/widgets/pie_chart_view.dart
@@ -1,5 +1,8 @@
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:home_space/constants.dart';
+import 'package:home_space/core/utils/colors.dart';
+import 'package:home_space/core/utils/styles.dart';
 
 class PieChartView extends StatefulWidget {
   const PieChartView({super.key});
@@ -13,11 +16,57 @@ class _PieChartViewState extends State<PieChartView> {
 
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(aspectRatio: 1, child: PieChart(getPieChartData()));
+    return AspectRatio(
+      aspectRatio: 1.5,
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          Positioned.fill(child: PieChart(getPieChartData())),
+          Text("\$896", style: AppStyles.bold14),
+        ],
+      ),
+    );
   }
 
   PieChartData getPieChartData() {
-    return PieChartData(pieTouchData: getPieTouchData());
+    return PieChartData(
+      pieTouchData: getPieTouchData(),
+      sectionsSpace: 0,
+      sections: [
+        PieChartSectionData(
+          value: 500,
+          showTitle: false,
+          color: PieChartColorsData.systemPurple,
+          radius: currentSectionIndex == 0
+              ? kTouchedRadiusChart
+              : kNormalRadiusChart,
+        ),
+        PieChartSectionData(
+          value: 280,
+          showTitle: false,
+          color: PieChartColorsData.systemBlackRed,
+          radius: currentSectionIndex == 1
+              ? kTouchedRadiusChart
+              : kNormalRadiusChart,
+        ),
+        PieChartSectionData(
+          value: 66,
+          showTitle: false,
+          color: PieChartColorsData.systemGreen,
+          radius: currentSectionIndex == 2
+              ? kTouchedRadiusChart
+              : kNormalRadiusChart,
+        ),
+        PieChartSectionData(
+          value: 50,
+          showTitle: false,
+          color: PieChartColorsData.systemYellow,
+          radius: currentSectionIndex == 3
+              ? kTouchedRadiusChart
+              : kNormalRadiusChart,
+        ),
+      ],
+    );
   }
 
   PieTouchData getPieTouchData() {
