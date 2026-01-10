a4323e982201006ebb80d0925bfa21b7e5f860ed
nidhal khazene <nidhal.khazene@gmail.com>
2026-01-10 11:38:47 +0100

    settings_notifivations_view done

--- lib/features/settings/presentation/views/widgets/settings_notifications_view.dart
+++ lib/features/settings/presentation/views/widgets/settings_notifications_view.dart
@@ -1,12 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:home_space/constants.dart';
+import 'package:home_space/core/models/list_tile_model.dart';
+import 'package:home_space/core/utils/colors.dart';
 import 'package:home_space/core/utils/styles.dart';
 import 'package:home_space/core/widgets/custom_app_bar.dart';
+import 'package:home_space/core/widgets/custom_button.dart';
+import 'package:home_space/core/widgets/toggle_container_switch.dart';
 
 class SettingsNotificationsView extends StatelessWidget {
   const SettingsNotificationsView({super.key});
 
   static const routeName = "SettingsNotificationsView";
+  static final List<ListTileModel> saleList = [
+    ListTileModel(
+      title: "Get Recommendation on Sale Suggested Searches ",
+      subtitle:
+          "Receive Information about new listing based on your search history.",
+    ),
+    ListTileModel(
+      title: "For Saved Sale listing Updates",
+      subtitle:
+          "We will notify you when your saved listing has a status Change",
+    ),
+  ];
+  static final List<ListTileModel> rentList = [
+    ListTileModel(
+      title: "Get Recommendation on Rent Suggested Searches ",
+      subtitle:
+          "Receive Information about new listing based on your search history.",
+    ),
+    ListTileModel(
+      title: "For Saved Rent listing Updates",
+      subtitle:
+          "We will notify you when your saved listing has a status Change",
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -15,8 +43,89 @@ class SettingsNotificationsView extends StatelessWidget {
         child: Padding(
           padding: kPrimaryScreenPadding,
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               CustomAppBar(title: "Notifications", textStyle: AppStyles.bold16),
+              const SizedBox(height: 24),
+              Row(
+                children: [
+                  Expanded(
+                    child: CustomButton(
+                      textContent: "Push",
+                      textStyle: AppStyles.regular12.copyWith(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  Expanded(
+                    child: CustomButton(
+                      textContent: "Email",
+                      textStyle: AppStyles.regular12.copyWith(
+                        color: ColorsData.kMediumPrimaryColor,
+                      ),
+                      backgroundColor: Colors.transparent,
+                      borderColor: ColorsData.kMediumPrimaryColor,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 32),
+              Text(
+                'Sale Alert',
+                style: AppStyles.medium16.copyWith(
+                  color: ColorsData.kFontSecondaryColor,
+                ),
+              ),
+              const SizedBox(height: 16),
+              Column(
+                spacing: 16,
+                children: List.generate(
+                  saleList.length,
+                  (index) => ListTile(
+                    contentPadding: EdgeInsets.zero,
+                    title: Text(
+                      saleList[index].title!,
+                      style: AppStyles.medium12,
+                    ),
+                    subtitle: Text(
+                      saleList[index].subtitle!,
+                      style: AppStyles.regular12.copyWith(
+                        color: ColorsData.kFontSecondaryColor,
+                      ),
+                    ),
+                    trailing: const ToggleContainerSwitch(),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 24),
+              Text(
+                'Rent Alert',
+                style: AppStyles.medium16.copyWith(
+                  color: ColorsData.kFontSecondaryColor,
+                ),
+              ),
+              const SizedBox(height: 16),
+              Column(
+                spacing: 16,
+                children: List.generate(
+                  rentList.length,
+                  (index) => ListTile(
+                    contentPadding: EdgeInsets.zero,
+                    title: Text(
+                      rentList[index].title!,
+                      style: AppStyles.medium12,
+                    ),
+                    subtitle: Text(
+                      rentList[index].subtitle!,
+                      style: AppStyles.regular12.copyWith(
+                        color: ColorsData.kFontSecondaryColor,
+                      ),
+                    ),
+                    trailing: const ToggleContainerSwitch(),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
