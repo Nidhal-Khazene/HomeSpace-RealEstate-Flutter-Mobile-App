4e51163dc6bfc65db9f1c131e1adb6d46c3c47ef
nidhal khazene <nidhal.khazene@gmail.com>
2026-01-12 11:11:25 +0100

    custom app bar: add profile image option

--- lib/core/widgets/custom_app_bar.dart
+++ lib/core/widgets/custom_app_bar.dart
@@ -3,16 +3,21 @@ import 'package:home_space/constants.dart';
 import 'package:home_space/core/utils/styles.dart';
 import 'package:home_space/core/widgets/custom_go_back_button.dart';
 
+import 'custom_circle_container.dart';
+
 class CustomAppBar extends StatelessWidget {
   const CustomAppBar({
     super.key,
     required this.title,
     this.textStyle,
     this.widthFromBackButton,
+    this.profileImageWidget,
   });
+
   final String title;
   final TextStyle? textStyle;
   final double? widthFromBackButton;
+  final Widget? profileImageWidget;
 
   @override
   Widget build(BuildContext context) {
@@ -24,7 +29,20 @@ class CustomAppBar extends StatelessWidget {
             Navigator.pop(context);
           },
         ),
-        Text(title, style: textStyle ?? AppStyles.medium16),
+        Row(
+          children: [
+            Visibility(
+              visible: profileImageWidget != null,
+              child: CustomCircleContainer(
+                backgroundColor: Colors.transparent,
+                borderColor: Colors.transparent,
+                widget: profileImageWidget,
+              ),
+            ),
+            const SizedBox(width: 10),
+            Text(title, style: textStyle ?? AppStyles.medium16),
+          ],
+        ),
         Flexible(
           child: SizedBox(
             width:
