5f689409d24aa6e0eb119500dc8020eec1eed460
nidhal khazene <nidhal.khazene@gmail.com>
2026-01-14 17:15:57 +0100

    chats message bar: add plus icon

--- lib/features/chats/presentation/views/widgets/chats_message_bar.dart
+++ lib/features/chats/presentation/views/widgets/chats_message_bar.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:home_space/core/utils/colors.dart';
 import 'package:home_space/core/utils/styles.dart';
+import 'package:home_space/core/widgets/custom_oval_container.dart';
 import 'package:home_space/core/widgets/custom_text_field.dart';
 
 class ChatsMessageBar extends StatelessWidget {
@@ -12,13 +13,28 @@ class ChatsMessageBar extends StatelessWidget {
       child: Row(
         children: [
           Expanded(
-            child: CustomTextField(
-              hintStyle: AppStyles.regular12.copyWith(
-                color: ColorsData.kFontSecondaryColor,
+            child: SizedBox(
+              height: 40,
+              child: CustomTextField(
+                hintStyle: AppStyles.regular12.copyWith(
+                  color: ColorsData.kFontSecondaryColor,
+                ),
+                textContent: "Write Message",
+                borderRadiusValue: 100,
               ),
-              textContent: "Write Message",
-              borderRadiusValue: 100,
             ),
+          ),
+          const SizedBox(width: 11),
+          CustomOvalContainer(
+            height: 40,
+            width: 40,
+            widget: Icon(
+              Icons.add,
+              color: ColorsData.kMediumPrimaryColor,
+              size: 20,
+            ),
+            borderColor: ColorsData.kBorderColor,
+            backgroundColor: Colors.transparent,
           ),
         ],
       ),
