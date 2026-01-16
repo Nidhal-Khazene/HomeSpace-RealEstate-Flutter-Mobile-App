cc35932e735a5930e9d186442dd63cf24a1bcbc1
nidhal khazene <nidhal.khazene@gmail.com>
2026-01-16 09:35:52 +0100

    add custom add media structure

---
+++ lib/core/widgets/custom_add_media.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class CustomAddMedia extends StatelessWidget {
+  const CustomAddMedia({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class ActiveAddingMedia extends StatelessWidget {
+  const ActiveAddingMedia({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class NoActiveAddingMedia extends StatelessWidget {
+  const NoActiveAddingMedia({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
